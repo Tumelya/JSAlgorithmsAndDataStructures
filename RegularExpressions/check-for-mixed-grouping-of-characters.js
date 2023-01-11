6bed90bf3b766661991feb193266387d5947c8ed
Mariya Tumelya <mtumelia@gmail.com>
2023-01-11 11:46:29 +0300

    feat: add the check-for-mixed-grouping-of-characters solution

---
+++ RegularExpressions/check-for-mixed-grouping-of-characters.js
@@ -0,0 +1,8 @@
+//Fix the regex so that it checks for the names of Franklin Roosevelt or Eleanor Roosevelt
+// in a case sensitive manner and it should make concessions for middle names.
+//Then fix the code so that the regex that you have created is checked against myString
+//and either true or false is returned depending on whether the regex matches.
+
+let myString = "Eleanor Roosevelt";
+let myRegex = /(Franklin|Eleanor) (([A-Z]\.?|[A-Z][a-z]+) )?Roosevelt/gi;
+let result = myRegex.test(myString); 
