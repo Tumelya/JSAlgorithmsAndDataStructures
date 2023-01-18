67de2424283970e1848a3617c97ab5fb955d8c2f
Mariya Tumelya <mtumelia@gmail.com>
2023-01-18 12:06:44 +0300

    refactor pig-latin solution

--- IntermediateAlgorithmScripting/pig-latin.js
+++ IntermediateAlgorithmScripting/pig-latin.js
@@ -1,14 +1,14 @@
 function translatePigLatin(str) {
     let regexVowels = /^([aeiou])/;
     if (str.match(regexVowels)) {
-        console.log(str + "way");
+        //console.log(str + "way");
         return str + "way";
     } else {
         let regexCons = /(^[^aeiou]+)/g;
         let consonants = str.match(regexCons);
-        console.log(consonants);
-        console.log(consonants[0].length);
-        console.log(str.slice(consonants[0].length) + consonants[0] + "ay");
+        //console.log(consonants);
+        //console.log(consonants[0].length);
+        //console.log(str.slice(consonants[0].length) + consonants[0] + "ay");
         return str.slice(consonants[0].length) + consonants[0] + "ay";
     }
 }
